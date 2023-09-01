74f16b7ba7d1fbac0cac6001366a0e3729938747
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-09-01 11:12:12 -0500

    added if statement to alert if there is an input field missing

--- client.js
+++ client.js
@@ -1,22 +1,33 @@
+let totalSalary = 0;
+
+
 $(document).ready(readyNow);
 function readyNow(){
     addToTable();
     $("#tableBody").on("click", ".deleteButton", onDelete);
+    calculateMonthlyTotal();
 }
 function addToTable(){
+    
         $(".submitButton").on("click", function(){
         const firstName = $("#firstName").val();
         const lastName = $("#lastName").val();
         const idNumber = $("#idNumber").val();
         const jobTitle = $("#jobTitle").val();
         const annualSalary = $("#annualSalary").val();
+    
+        if(!firstName || !lastName || !idNumber || !jobTitle || !annualSalary){
+            alert('Missing input field')
+            return;
+           }
+
         $("#tableBody").append(`      
         <tr>
             <td>${firstName}</td>
             <td>${lastName}</td>
             <td>${idNumber}</td>
             <td>${jobTitle}</td>
-            <td>${annualSalary}</td>
+            <td id ="salary" >${annualSalary}</td>
             <td><button type ="button" class="deleteButton">Delete</button></td>
           </tr>`);
         $("#firstName").val("");
@@ -25,9 +36,15 @@ function addToTable(){
         $("#jobTitle").val("");
         $("#annualSalary").val("");
         });
+
     }
 
     function onDelete(event) {
         const button = event.target;
         $(button).closest("tr").remove();
       }
+
+    function calculateMonthlyTotal(){
+        
+
+    }
