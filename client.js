6392151c852d86c6038be06a6180472880b9cecc
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-08-31 19:23:56 -0500

    got a working function for the submit button. Takes the input field information and appends it into the table

--- client.js
+++ client.js
@@ -1,5 +1,25 @@
 $(document).ready(readyNow);
-
-function readyNow() {
-  
+function readyNow(){
+    addToTable();
 }
+function addToTable(){ //This flashes on the screen then goes blank
+    console.log("wtf");
+        $(".submitButton").on("click", function(){
+        const firstName = $("#firstName").val();
+        const lastName = $("#lastName").val();
+        const idNumber = $("#idNumber").val();
+        const jobTitle = $("#jobTitle").val();
+        const annualSalary = $("#annualSalary").val();
+        console.log(firstName, lastName, idNumber, jobTitle, annualSalary);
+        $("#tableBody").append(`      
+        <tr>
+            <td>${firstName}</td>
+            <td>${lastName}</td>
+            <td>${idNumber}</td>
+            <td>${jobTitle}</td>
+            <td>${annualSalary}</td>
+          </tr>`);
+        });
+    }
+
+    
